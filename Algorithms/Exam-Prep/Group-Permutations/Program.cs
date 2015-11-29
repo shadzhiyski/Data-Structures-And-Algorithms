b4898f993a30c6662669c6b46f304792d0a0199c
Stoian Hadzhiyski <stoian2662@gmail.com>
2015-11-29 22:44:34 +0200

    Exam Preparation solution added.

---
+++ Algorithms/Exam-Prep/Group-Permutations/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace Group_Permutations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    class Program
+    {
+        private static int[] occurencies;
+        private static Dictionary<char, int> letterOccurencies = new Dictionary<char, int>();
+        private static StringBuilder result = new StringBuilder();
+        private static char[] letters;
+
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if(!letterOccurencies.ContainsKey(input[i]))
+                {
+                    letterOccurencies.Add(input[i], 0);
+                }
+
+                letterOccurencies[input[i]]++;
+            }
+
+            int n = letterOccurencies.Keys.Count;
+            int[] indices = Enumerable.Range(0, n).ToArray();
+
+            letters = input.ToCharArray().Distinct().ToArray();
+            Permute(indices);
+            Console.Write(result.ToString());
+        }
+
+        private static void Permute(int[] array, int startIndex = 0)
+        {
+            if (startIndex >= array.Length - 1)
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    int letterIndex = array[i];
+                    char letter = letters[letterIndex];
+                    result.Append(letter, letterOccurencies[letter]);
+                }
+
+                result.AppendLine();
+            }
+            else
+            {
+                for (int i = startIndex; i < array.Length; i++)
+                {
+                    Swap(i, startIndex, ref array);
+                    Permute(array, startIndex + 1);
+                    Swap(startIndex, i, ref array);
+                }
+            }
+        }
+
+        private static void Swap(int i, int startIndex, ref int[] array)
+        {
+            if (i != startIndex)
+            {
+                int temp = array[i];
+                array[i] = array[startIndex];
+                array[startIndex] = temp;
+            }
+        }
+    }
+}
---
+++ Algorithms/Exam-Prep/Line-Inverter/Program.cs
@@ -0,0 +1,169 @@
+﻿namespace Line_Inverter
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int matrixSize = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[matrixSize, matrixSize];
+            for (int row = 0; row < matrixSize; row++)
+            {
+                string input = Console.ReadLine();
+                Parallel.For(0, input.Length, (i) => 
+                {
+                    matrix[row, i] = input[i];
+                });
+                //for (int i = 0; i < input.Length; i++)
+                //{
+                    
+                //}
+            }
+
+            int steps = ClearMatrix(matrix);
+            Console.WriteLine(steps);
+        }
+
+        private static int ClearMatrix(char[,] matrix)
+        {
+            int stepsCount = 0;
+
+            while(!IsClear(matrix))
+            {
+                int[] rowInvertionsGain, colInvertionsGain;
+                FindInvertionsGains(matrix, out rowInvertionsGain, out colInvertionsGain);
+                int bestRow = FindMostInvertionsGainingRow(rowInvertionsGain);
+                int bestCol = FindMostInvertionsGainingCol(colInvertionsGain);
+
+                stepsCount++;
+                if (bestRow == -1 && bestCol == -1) { return -1; }
+
+                if (bestRow >= bestCol)
+                {
+                    InvertRow(matrix, bestRow);
+                } else
+                {
+                    InvertCol(matrix, bestCol);
+                }
+            }
+
+            return stepsCount;
+        }
+
+        private static int FindMostInvertionsGainingCol(int[] colInvertionsGain)
+        {
+            int bestCol = -1, maxInvertionsGain = -1;
+            for (int i = 0; i < colInvertionsGain.Length; i++)
+            {
+                if (colInvertionsGain[i] > maxInvertionsGain)
+                {
+                    maxInvertionsGain = colInvertionsGain[i];
+                    bestCol = i;
+                }
+            }
+
+            return bestCol;
+        }
+
+        private static int FindMostInvertionsGainingRow(int[] rowInvertionsGain)
+        {
+            int bestRow = -1, maxInvertionsGain = -1;
+            for (int i = 0; i < rowInvertionsGain.Length; i++)
+            {
+                if(rowInvertionsGain[i] > maxInvertionsGain)
+                {
+                    maxInvertionsGain = rowInvertionsGain[i];
+                    bestRow = i;
+                }
+            }
+
+            return bestRow;
+        }
+
+        private static void InvertCol(char[,] matrix, int bestCol)
+        {
+            Parallel.For(0, matrix.GetLength(0), (row) => 
+            {
+                if (matrix[row, bestCol] == 'W')
+                {
+                    matrix[row, bestCol] = 'B';
+                }
+                else
+                {
+                    matrix[row, bestCol] = 'W';
+                }
+            });
+            //for (int row = 0; row < matrix.GetLength(0); row++)
+            //{
+                
+            //}
+        }
+
+        private static void InvertRow(char[,] matrix, int bestRow)
+        {
+            Parallel.For(0, matrix.GetLength(1), (col) => 
+            {
+                if (matrix[bestRow, col] == 'W')
+                {
+                    matrix[bestRow, col] = 'B';
+                }
+                else
+                {
+                    matrix[bestRow, col] = 'W';
+                }
+            });
+            //for (int col = 0; col < matrix.GetLength(1); col++)
+            //{
+                
+            //}
+        }
+
+        private static void FindInvertionsGains(
+            char[,] matrix, out int[] rowInvertionsGain, out int[] colInvertionsGain)
+        {
+            rowInvertionsGain = new int[matrix.GetLength(0)];
+            colInvertionsGain = new int[matrix.GetLength(1)];
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if(matrix[row, col] == 'W')
+                    {
+                        rowInvertionsGain[row]++;
+                        colInvertionsGain[col]++;
+                    }
+                    else
+                    {
+                        rowInvertionsGain[row]--;
+                        colInvertionsGain[col]--;
+                    }
+                }
+            }
+        }
+
+        private static bool IsClear(char[,] matrix)
+        {
+            int result = 0;
+            Parallel.For(0, matrix.GetLength(0), (row) => 
+            {
+                Parallel.For(0, matrix.GetLength(1), (col) => 
+                {
+                    if (matrix[row, col] == 'W')
+                    { result++; }
+                });
+            });
+            //for (int row = 0; row < matrix.GetLength(0); row++)
+            //{
+                //for (int col = 0; col < matrix.GetLength(1); col++)
+                //{
+
+                //}
+            //}
+
+            return result == 0;
+        }
+    }
+}
